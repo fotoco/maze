8e9c1425a34e8b4ba3a16753dd49a3862bb89d07
fotoco <fotoco@naver.com>
2015-05-16 00:16:29 +0900

    기울기 센스 추가.
    기울기 센스 추가.

--- Assets/Script/GravityController.cs
+++ Assets/Script/GravityController.cs
@@ -3,16 +3,26 @@ using System.Collections;
 
 public class GravityController : MonoBehaviour {
 
+
+	private Vector3 accel;
+
+
 	// Use this for initialization
 	void Start () {
-	
+		accel = new Vector3(0, 0, -1);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Physics.gravity = Quaternion.AngleAxis(Input.GetAxis("Horizontal") * 60, Vector3.forward) *
-			Quaternion.AngleAxis(Input.GetAxis("Vertical") * -60, Vector3.right) *
-			(Vector3.up * -20);
+		//Physics.gravity = Quaternion.AngleAxis(Input.GetAxis("Horizontal") * 60, Vector3.forward) *
+		//    Quaternion.AngleAxis(Input.GetAxis("Vertical") * -60, Vector3.right) *
+		//    (Vector3.up * -20);
+
+
+
+
+		accel = (accel + Input.acceleration) * 0.5f;
+		Physics.gravity = new Vector3(-accel.y, accel.z, accel.x) * 50;
 
 	}
 }
--- Assets/Script/TiltRig.cs
+++ Assets/Script/TiltRig.cs
@@ -3,15 +3,28 @@ using System.Collections;
 
 public class TiltRig : MonoBehaviour {
 
+
+	private Vector3 accel;
+
 	// Use this for initialization
 	void Start () {
-	
+		accel = new Vector3(0, 0, -1);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.rotation =
-			Quaternion.AngleAxis(Input.GetAxis("Horizontal") * 10, Vector3.forward) *
-			Quaternion.AngleAxis(Input.GetAxis("Vertical") * -10, Vector3.right);
+		//transform.rotation =
+		//    Quaternion.AngleAxis(Input.GetAxis("Horizontal") * 10, Vector3.forward) *
+		//    Quaternion.AngleAxis(Input.GetAxis("Vertical") * -10, Vector3.right);
+
+
+
+		accel = (accel + Input.acceleration) * 0.5f;
+		Vector3 gravity = new Vector3(-accel.y, accel.z, accel.x);
+
+		transform.rotation = Quaternion.FromToRotation(-Vector3.up, gravity);
+
+
+
 	}
 }
